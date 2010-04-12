91e4d7b9e8c2ae8fe86ee4ce7994bc12e038aea7
Alaor Neto <alaorneto@gmail.com>
2010-04-12 16:57:29 -0300

    Alteracoes na parte de deteccao de ondas

---
+++ ECG.Graphics/ComplexoQRS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace ECG.Graphics
+{
+    public class ComplexoQRS
+    {
+        OndaHelper helper;
+        double[][] qrs;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="imagem"></param>
+        public ComplexoQRS(Bitmap imagem)
+        {
+            helper = new OndaHelper(imagem);
+
+            int[] picosQRS = helper.PicosQRS();
+
+            qrs = new double[picosQRS.Length][];
+
+            for(int i = 0; i < picosQRS.Length; i++)
+            {
+                int inicio, fim;
+                int[] extremos = helper.ExtremosQRS(picosQRS[i]);
+                inicio = extremos[0];
+                fim = extremos[1];
+                qrs[i] = helper.SubVetor(inicio, fim);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public double[] this[int index]
+        {
+            get { return qrs[index]; }
+        }
+    }
+}
---
+++ ECG.Graphics/ECGOnda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace ECG.Graphics
+{
+    /// <summary>
+    /// Esta classe encapsula um Bitmap representando a derivação do exame a ser analisada e provê funcções
+    /// úteis para a transformação dessa imagem para outras formas de representação
+    /// </summary>
+    public class ECGOnda
+    {
+        public ComplexoQRS ComplexoQRS;
+        private OndaHelper helper;
+        private Bitmap bitmap;
+        private double[] vetor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="imagem">O objeto Bitmap com a imagem da onda a ser trabalhada</param>
+        public ECGOnda(Bitmap imagem)
+        {
+            bitmap = imagem;
+
+            helper = new OndaHelper(imagem);
+
+            vetor = helper.GerarVetor();
+
+            ComplexoQRS = new ComplexoQRS(imagem);
+        }
+
+        public double this[int index]
+        {
+            get { return vetor[index]; }
+        }
+
+        public int Lenght
+        {
+            get { return vetor.Length; }
+        }
+
+        public Bitmap Bitmap
+        {
+            get { return this.bitmap; }
+        }
+    }
+}
